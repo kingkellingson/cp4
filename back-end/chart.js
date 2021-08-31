9faf83df25d417b4f17ef89703d7ccb8e3a01023
kyle <kingkellingson@gmail.com>
2021-08-31 17:23:23 -0600

    test

---
+++ back-end/chart.js
@@ -0,0 +1,31 @@
+// quickchart-js https://github.com/typpo/quickchart-js
+
+const QuickChart = require('quickchart-js');
+
+const chart = new QuickChart();
+
+chart.setWidth(500)
+chart.setHeight(300);
+
+chart.setConfig({
+  type: 'bar',                                // Show a bar chart
+  data: {
+    labels: [2012, 2013, 2014, 2015, 2016],   // Set X-axis labels
+    datasets: [{
+      label: 'Users',                         // Create the 'Users' dataset
+      data: [120, 60, 50, 180, 120]           // Add data to the chart
+    }]
+  }
+});
+
+// Print the chart URL
+console.log(chart.getUrl());
+
+// Get the image...
+const image = await chart.toBinary();
+
+// Or write it to a file
+chart.toFile('chart.png');
+
+// Print the chart URL
+console.log(myChart.getUrl());
---
+++ back-end/countries.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const router = express.Router();
+const users = require("./users.js");
