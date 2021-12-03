de140424701460c5e948d55e0f74275ca0e3c3e8
Kyle Ellingson <kingkellingson@gmail.com>
2021-12-03 14:52:26 -0700

    Got back Dashboard to get it to work

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Dashboard from '../views/Dashboard.vue'
 import Photo from '../views/Photo.vue'
 
 Vue.use(VueRouter);
@@ -10,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   },
   {
     path: '/photo/:id',
